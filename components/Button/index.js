fcca556e88b520dd17abacc418bdeaa5f84ea663
Askhat <baltabaev.a2509@gmail.com>
2019-11-15 14:51:58 +0600

    feat: some project structure completion

--- components/Button/index.js
+++ components/Button/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Btn } from './style';
 
-const Button = () => {
-  return <Btn>hello</Btn>;
+const Button = props => {
+  return <Btn width={props.width}>{props.children}</Btn>;
+};
+
+Button.propTypes = {
+  width: PropTypes.string,
+  children: PropTypes.string
 };
 
 export default Button;
