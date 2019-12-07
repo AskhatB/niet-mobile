8c6470a277e43534ded6bfe2c81fca37a585aedf
Askhat <baltabaev.a2509@gmail.com>
2019-12-07 12:46:05 +0600

    feat: text

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -6,7 +6,12 @@ const Footer = () => {
   return (
     <Wrap>
       <LogoIcon></LogoIcon>
-      <Copyright>© Copyright 2019, niet.kz, официальный сайт.</Copyright>
+      <Copyright>
+        © Copyright 2019, niet.kz, официальный сайт.
+        <br />
+        Услуги страхования оказывает АО «СК «Freedom Finance Insurance».
+        Лицензия №т2.1.72 от 06.08.2019 года.
+      </Copyright>
     </Wrap>
   );
 };
--- components/Footer/style.js
+++ components/Footer/style.js
@@ -4,7 +4,7 @@ export const Wrap = styled.div`
   margin-top: 20px;
   padding: 0 16px;
   box-sizing: border-box;
-  height: 180px;
+  height: 220px;
   position: relative;
   background: ${({ theme }) => theme.colors.gray};
 `;
